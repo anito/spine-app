cedd396c5071a2c4ee407f5d48099202d53bda1c
anito <axelnitzschner@mac.com>
2020-04-23 16:50:18 +0200

    added npm scripts for hem

--- assets/spine/build.sh
+++ assets/spine/build.sh
@@ -1,2 +1,21 @@
-#!bin/bash
-hem
+#!/bin/bash
+source ~/.bash_profile
+
+#!/bin/bash
+for i in "$@"
+do
+case $i in
+    *)
+    cmd="${i#*=}"
+    shift # past argument=value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+hem "${cmd}"
