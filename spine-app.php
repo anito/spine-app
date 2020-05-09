9c236a7d03a1e6582408ba4b5c2757f8d09f7d5d
anito <axelnitzschner@mac.com>
2020-05-09 16:52:57 +0200

    bumped to version 1.2.5

--- spine-app.php
+++ spine-app.php
@@ -3,7 +3,7 @@
 Plugin Name: Spine App
 Plugin URI: https://webpremiere.de
 Description: Extend Wordpress by SpineJS
-Version: 1.2.4
+Version: 1.2.5
 Author: Webpremiere
 Author URI: https://webpremiere.de
 Text Domain: spine-app
